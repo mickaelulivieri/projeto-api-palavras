65be172e7064a4f10fde2eef50a40117783925e4
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-11-24 13:42:47 -0300

    first commit

---
+++ auth-service/src/test/java/com/projeto/microservicos/palavras/PalavrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.microservicos.palavras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalavrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
